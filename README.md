a5f9699b6ee09735ecb64b23988e929681ac373b
Soorya Narayan <nayaran.ayroos@gmail.com>
2016-10-11 11:32:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# {CoolName}
+
+{CoolName} is a TCP/IP stack implemented on the linux user space using TUN/TAP devices.
---
+++ stdheaders.h
@@ -0,0 +1,24 @@
+#ifndef ALLHEAD
+#define ALLHEAD
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <netdb.h>
+#include <pthread.h>
+
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+
+#endif
